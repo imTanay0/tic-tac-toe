f9d81fabf9aafa79b4e83b77716f24deb18aa8c7
imtanay <107740662+imTanay0@users.noreply.github.com>
2022-11-15 13:29:11 +0530

    removed unnecesary comments

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,55 +1,7 @@
-// import React from 'react';
-import { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
 export default function Board(props) {
-
-	// const [squares, setSquares] = useState(new Array(9).fill(null));
-	// const [xIsNext, setXIsNext] = useState(true)
-
-	// const calculateWinner = (squares) => {
-	// 	const lines = [
-	// 		[0, 1, 2],
-	// 		[3, 4, 5],
-	// 		[6, 7, 8],
-	// 		[0, 3, 6],
-	// 		[1, 4, 7],
-	// 		[2, 5, 8],
-	// 		[0, 4, 8],
-	// 		[2, 4, 6]
-	// 	];
-
-	// 	for (let i = 0; i < lines.length; i++) {
-	// 		const [a, b, c] = lines[i];
-	// 		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-	// 			return squares[a];
-	// 		}
-	// 	}
-
-	// 	return null;
-	// }
-
-	// const winner = calculateWinner(squares);
-	// let status;
-
-	// if (winner) {
-	// 	status = "Winner: " + winner;
-	// } else {
-	// 	status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-	// }
-
-	// function handleClick(i) {
-	// 	const newSquares = squares.slice();
-		
-	// 	if (calculateWinner(newSquares) || newSquares[i]) {
-	// 		return;
-	// 	}
-		
-	// 	newSquares[i] = xIsNext ? 'X' : 'O'
-		
-	// 	setSquares(newSquares)
-	// 	setXIsNext(!xIsNext)
-	// }
 
 	function renderSquare(i) {
 		return (<Square
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-import { render } from "react-dom";
 import Board from "./Board";
-import Square from "./Square";
 
 export default function Game() {
 	const [history, setHistory] = useState([
